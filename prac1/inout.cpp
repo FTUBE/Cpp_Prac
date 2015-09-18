8238670516e8cc934c9e059b5bf8f8701ba427eb
FTUBE <tian.yang14@imperial.ac.uk>
2015-09-18 14:34:48 +0800

    Init

---
+++ prac1/inout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number;
+  char character;
+  cout << "Type in a character:\n";
+  cin >> character;
+  number = character;
+  cout << "The character '" << character << "\n";
+
+  return 0;
+}
